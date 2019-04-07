b0edfa851f2616630cf400b49ae27c91eaa52f8c
lemarwin <lemarwin42@gmail.com>
2019-04-07 18:23:11 +0300

    router semigroup

--- src/Web/Reflex/Bootstrap/Router.hs
+++ src/Web/Reflex/Bootstrap/Router.hs
@@ -13,9 +13,11 @@ newtype Route t m a = Route { unRoute :: Event t (m (a, Route t m a)) }
 instance (Reflex t, Functor m) => Functor (Route t m) where
   fmap f (Route e) = Route $ fmap (\(a, r) -> (f a, f <$> r)) <$> e
 
+instance Reflex t => Semigroup (Route t m a) where
+  (Route e1) <> (Route e2) = Route $ leftmost [e1, e2]
+
 instance Reflex t => Monoid (Route t m a) where
   mempty = Route never
-  (Route e1) `mappend` (Route e2) = Route $ leftmost [e1, e2]
 
 -- | Run widget that can replace itself with new widget constructed
 -- internally in the original widget.
